2605fad45c9bc2855f5310eb40256fb6e7d20742
ko_cha78 <kota1725@gmail.com>
2021-07-06 17:15:59 +0900

    2

--- lib/main.dart
+++ lib/main.dart
@@ -36,60 +36,66 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         body: Stack(
           children: [
+            //Image.asset('assets/b.jpg'),
             Positioned.fill(child: Image.asset('assets/a.jpg', fit: BoxFit.fill,),),
             Column(children: [
-              Expanded(child: (Container())),
-              Text("The time to\nact is now", textAlign: TextAlign.center, style: TextStyle(fontSize: 30.0,color: Colors.white)),
+              Expanded(flex:1 ,child: (Container())),
+              Text("The time to\nact is now", textAlign: TextAlign.center, style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold)),
               //Image.asset("assets/350x350.jpg"),
-              Text("Your path is laid out in front of\nyou.", textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, color: Colors.white)),
+              Expanded(flex:3,child: (Container())),
+              Text("Your path is laid out in front of\nyou. By getting started, you\nalready took the biggest step in\nself-improvement.\nWe will guide you through it\n",
+                  textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical:4,horizontal:8),
-                child: Material(
-                  color: Colors.blue,
-                  child: Ink(
-                      decoration:BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                      ),
-                      child: InkWell(
-                        onTap:() {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SecondRoute()),
-                          );
-                        },
-                        child: Container(
-                          height: 50,
-                          child: Stack(
-                            children: [
-                              Row(
-                                  children:[
-                                    SizedBox(width: 16,),
-                                    Center(child: Icon(Icons.send),),
-                                  ]
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Center(
-                                      child: Text(
-                                        'Continue with Google',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            color: Colors.white
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  decoration: BoxDecoration(gradient: LinearGradient(
+                    colors: [Colors.pink[400], Colors.pink[200]],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),borderRadius: BorderRadius.all(Radius.circular(10.0)),),
+                  child: Material(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    child: Ink(
+                        decoration:BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        child: InkWell(
+                          onTap:() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SecondRoute()),
+                            );
+                          },
+                          child: Container(
+                            height: 50,
+                            child: Stack(
+                              children: [
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          'Continue',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.white
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              )
-                            ],
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      )
+                        )
+                    ),
                   ),
                 ),
               ),
-              Expanded(child: (Container())),
             ],
           ),]
         )
